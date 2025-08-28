b9ebbe6bfb3a3e3969d607fd270159d082e21364
Ujjwal Panchal <panchalujjwal520@gmail.com>
2025-08-28 14:08:30 +0530

    deeploy

--- Frontend/next.config.mjs
+++ Frontend/next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output:'export',
+  eslint:{
+    ignoreDuringBuilds: true,
+  },
   experimental: {
     serverActions: {
       bodySizeLimit: '2mb'
